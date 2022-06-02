b2399f85a020195df37a076d4eaa02f04cbf2aa7
Fotoc <102552189+Fotoc@users.noreply.github.com>
2022-06-02 16:32:13 +0200

    Add files via upload

---
+++ Futbol/Futbol_app.java
@@ -0,0 +1,14 @@
+package Futbol;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Futbol_app {
+
+    public static void main(String args[]) throws SQLException, IOException {
+        Futbol f = new Futbol();
+
+        f.gestionarEquip();
+
+    }
+}
