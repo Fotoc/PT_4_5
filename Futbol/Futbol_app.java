090173ff6207dfa8d2784d57c214f29062c8806a
Fotoc <102552189+Fotoc@users.noreply.github.com>
2022-06-02 18:43:50 +0200

    Add files via upload

--- Futbol/Futbol_app.java
+++ Futbol/Futbol_app.java
@@ -1,6 +1,9 @@
 package Futbol;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.sql.SQLException;
 
 public class Futbol_app {
@@ -8,6 +11,21 @@ public class Futbol_app {
     public static void main(String args[]) throws SQLException, IOException {
         Futbol f = new Futbol();
 
+        File club = new File("club.dat");
+        try {
+
+            FileOutputStream fos = new FileOutputStream(club);
+            ObjectOutputStream dades = new ObjectOutputStream(fos);
+
+            dades.writeObject(soci);
+
+            dades.close();
+            fos.close();
+
+        } catch (Exception e) {
+            System.out.println("Error al afegir les dades al arxiu."
+                    + e.getMessage());
+        }
         f.gestionarEquip();
 
     }
